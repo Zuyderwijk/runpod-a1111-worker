e40e4e0e5e984408e5d4d248da63c84efd20f784
Jasper Zuyderwijk <zuyderwijk@gmail.com>
2025-05-25 01:10:22 +0200

    Add the symlinks to the runpod headlines API worker

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,6 +54,28 @@ wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/
 wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/config.json
 wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/ui-config.json
 
+echo "Setting up model symlinks to single source of truth"
+
+# Define base model folder (inside runpod-a1111-worker)
+MODEL_SRC="/workspace/runpod-a1111-worker/models"
+
+# Define target model folders inside stable-diffusion-webui
+MODEL_TARGET_BASE="/workspace/stable-diffusion-webui/models/Stable-diffusion"
+MODEL_TARGET_LORA="/workspace/stable-diffusion-webui/models/Lora"
+MODEL_TARGET_VAE="/workspace/stable-diffusion-webui/models/VAE"
+MODEL_TARGET_CONTROLNET="/workspace/stable-diffusion-webui/models/ControlNet"
+
+# Remove existing model folders (if any) and create symlinks
+rm -rf "$MODEL_TARGET_BASE" "$MODEL_TARGET_LORA" "$MODEL_TARGET_VAE" "$MODEL_TARGET_CONTROLNET"
+
+ln -s "$MODEL_SRC/Stable-diffusion" "$MODEL_TARGET_BASE"
+ln -s "$MODEL_SRC/Lora" "$MODEL_TARGET_LORA"
+ln -s "$MODEL_SRC/VAE" "$MODEL_TARGET_VAE"
+ln -s "$MODEL_SRC/ControlNet" "$MODEL_TARGET_CONTROLNET"
+
+echo "Symlinks created:"
+ls -l /workspace/stable-diffusion-webui/models
+
 echo "Starting Automatic1111 Web UI"
 deactivate
 export HF_HOME="/workspace"
